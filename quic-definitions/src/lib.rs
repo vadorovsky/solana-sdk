cdeecffafd3ee1982b681787380d02278ab0eca0
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2025-06-20 00:51:54 -0700

    Increase quic heartbeat interval to 45 second to reduce overhead (#201)

--- quic-definitions/src/lib.rs
+++ quic-definitions/src/lib.rs
@@ -22,9 +22,7 @@ pub const QUIC_MAX_TIMEOUT: Duration = Duration::from_secs(60);
 
 /// To avoid idle timeout, the QUIC endpoint sends a ping every
 /// QUIC_KEEP_ALIVE. This shouldn't be too low to avoid unnecessary ping traffic.
-/// For upgrade purpose, we keep the original one. Once the network is upgraded
-/// to the one having higher QUIC_MAX_TIMEOUT, this value can be increased.
-pub const QUIC_KEEP_ALIVE: Duration = Duration::from_secs(1);
+pub const QUIC_KEEP_ALIVE: Duration = Duration::from_secs(45);
 
 // Disable Quic send fairness.
 // When set to false, streams are still scheduled based on priority,
