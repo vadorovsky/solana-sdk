f6025a9a23c3f29fca1dfc5b79c9b44efc22b6ab
Jon C <me@jonc.dev>
2025-10-17 02:18:21 +0200

    sha256-hasher: Make it no-std compatible (#384)
    * sha256-hasher: Make it no-std compatible
    
    #### Problem
    
    The sha256-hasher doesn't compile in real no-std environments.
    
    #### Summary of changes
    
    This was an easy fix, simply don't pull in default features for the sha2
    crate. This means we disable OIDs for hashes, and the ability to
    finalize hashes into Box'ed values. Since we don't use that, it should
    be safe to disable.
    
    Also, the crate was a no-op to build without the sha2 feature, so I
    changed the no-std checking strategy to *exclude* features rather than
    include certain features, and moved it to `cargo hack` so that each
    crate and feature is tested individually.
    
    * Install cargo-hack in CI

--- scripts/check-no-std.sh
+++ scripts/check-no-std.sh
@@ -23,6 +23,7 @@ no_std_crates=(
   -p solana-rent
   -p solana-sanitize
   -p solana-sdk-ids
+  -p solana-sha256-hasher
   -p solana-signature
   -p solana-sysvar-id
   -p solana-system-interface
@@ -31,14 +32,22 @@ no_std_crates=(
 # no_std support really works.
 target="bpfel-unknown-none"
 
-./cargo nightly check -Zbuild-std=core \
+# These features require alloc
+exclude_features_no_alloc="alloc,borsh,curve25519,serde"
+# These features never work on upstream BPF
+exclude_features="atomic,bincode,default,dev-context-only-utils,frozen-abi,rand,std,verify"
+
+./cargo nightly hack check \
+  -Zbuild-std=core \
   "--target=$target" \
-  --no-default-features \
+  "--exclude-features=${exclude_features},${exclude_features_no_alloc}" \
+  --each-feature \
   "${no_std_crates[@]}"
 
 # Check that all crates with features that work with no_std + alloc still work!
-./cargo nightly check -Zbuild-std=alloc,core \
+./cargo nightly hack check \
+  -Zbuild-std=alloc,core \
   "--target=${target}" \
-  --no-default-features \
-  --features "decode, error, sanitize, syscalls, borsh, serde, bytemuck, alloc" \
+  "--exclude-features=${exclude_features}" \
+  --each-feature \
   "${no_std_crates[@]}"
