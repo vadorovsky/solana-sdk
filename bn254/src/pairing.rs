cbe1dda09fc150a4d9e2d95b48273327ffa66af8
samkim-crypto <skim13@cs.stanford.edu>
2025-10-23 20:01:05 +0900

    [bn254] Revert breaking change on `alt_bn128_pairing` (#399)
    revert breaking change on `alt_bn128_pairing`

--- bn254/src/pairing.rs
+++ bn254/src/pairing.rs
@@ -146,9 +146,37 @@ pub fn alt_bn128_pairing_be(input: &[u8]) -> Result<Vec<u8>, AltBn128Error> {
 }
 
 #[deprecated(since = "3.1.0", note = "Please use `alt_bn128_pairing_be` instead")]
+#[allow(deprecated)]
 #[inline(always)]
 pub fn alt_bn128_pairing(input: &[u8]) -> Result<Vec<u8>, AltBn128Error> {
-    alt_bn128_pairing_be(input)
+    #[cfg(not(target_os = "solana"))]
+    {
+        alt_bn128_versioned_pairing(VersionedPairing::V0, input, Endianness::BE)
+    }
+    #[cfg(target_os = "solana")]
+    {
+        if input
+            .len()
+            .checked_rem(ALT_BN128_PAIRING_ELEMENT_LEN)
+            .is_none()
+        {
+            return Err(AltBn128Error::InvalidInputData);
+        }
+        let mut result_buffer = [0u8; 32];
+        let result = unsafe {
+            syscalls::sol_alt_bn128_group_op(
+                ALT_BN128_PAIRING,
+                input as *const _ as *const u8,
+                input.len() as u64,
+                &mut result_buffer as *mut _ as *mut u8,
+            )
+        };
+
+        match result {
+            0 => Ok(result_buffer.to_vec()),
+            _ => Err(AltBn128Error::UnexpectedError),
+        }
+    }
 }
 
 #[inline(always)]
